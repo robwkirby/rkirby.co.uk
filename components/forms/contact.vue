6e8d951a30f109c7e8267e602c10a778be969bbe
Rob <robkirby@live.co.uk>
2019-03-12 01:33:42 +0000

    Replaced formspree in contact form

--- components/forms/contact.vue
+++ components/forms/contact.vue
@@ -1,5 +1,5 @@
 <template>
-    <form action="https://formspree.io/hello@rkirby.co.uk" method="POST" class="contact-form__form" @submit.prevent="submit">
+    <form action="https://formcarry.com/s/sFRGyxTE_Zd" method="POST" accept-charset="UTF-8" class="contact-form__form" @submit.prevent="submit">
         <div class="row">
             <div class="col-12 col-lg-6">
                 <div class="contact-form__field" :class="{ 'contact-form__field--error': $v.name.$error }">
@@ -22,21 +22,23 @@
         </div>
         <div class="row">
             <div class="col-12">
-                <div class="contact-form__field contact-form__field--message">
+                <div class="contact-form__field contact-form__field--message" :class="{ 'contact-form__field--error': $v.message.$error }">
                     <label class="contact-form__label" for="message">Message</label>
                     <textarea class="contact-form__input contact-form__input--textarea" name="message" id="message" cols="30" rows="7" v-model.trim="message"></textarea>
 
                     <div class="contact-form__error" v-if="!$v.message.required">Field is required</div>
-                    <div class="contact-form__error" v-if="!$v.message.minLength">Name must have at least {{$v.message.$params.minLength.min}} letters.</div>
+                    <div class="contact-form__error" v-if="!$v.message.minLength">Message must have at least {{$v.message.$params.minLength.min}} letters.</div>
                 </div>
             </div>
         </div>
 
+        <input type="hidden" name="_gotcha">
+
         <div class="contact-form__field">
             <button class="btn contact-form__submit js-contact-btn">
                 <span>Send</span>
-                <div class="loader-wrap"><div class="loader"></div></div>
             </button>
+            <div class="loader-wrap"><div class="loader"></div></div>
         </div>
         <input type="hidden" name="_subject" value="Website contact" />
     </form>
@@ -67,7 +69,7 @@ export default {
         },
         message: {
             required,
-            minLength: minLength(4)
+            minLength: minLength(20)
         }
     },
     methods: {
@@ -88,7 +90,7 @@ export default {
 
                 axios({
                     method: 'post',
-                    url: 'https://formspree.io/hello@rkirby.co.uk',
+                    url: 'https://formcarry.com/s/sFRGyxTE_Zd',
                     data: bodyFormData,
                     config: { headers: {'Content-Type': 'multipart/form-data' }}
                 })
@@ -154,6 +156,28 @@ export default {
 
         &__submit {
             cursor: pointer;
+            position: relative;
+        }
+
+        .loader-wrap {
+            .loader {
+              
+                display: inline-block;
+                width: 50px;
+                height: 50px;
+                border: 3px solid rgba(255,255,255,.3);
+                border-radius: 50%;
+                border-top-color: #fff;
+                animation: spin 1s ease-in-out infinite;
+                }
+
+                @keyframes spin {
+                to { transform: rotate(360deg); }
+                }
+                @-webkit-keyframes spin {
+                to { transform: rotate(360deg); }
+                
+            }
         }
     }
 
